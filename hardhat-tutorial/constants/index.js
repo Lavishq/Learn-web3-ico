3937db28368b2ae0bf324afae06c97ae9d9f6ee5
Lavishq <v.whitehearted@simplelogin.co>
2022-08-17 19:58:40 +0530

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x1032382b1Cf2897Ff07a8FCAc316bbBD38fb27c5";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
